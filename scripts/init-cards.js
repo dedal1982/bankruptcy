04b4a2ce326f6490f8c410725d5086cc9ef1fc16
Al Dm <d44.a1@yandex.ru>
2025-12-02 00:31:55 +0300

    update index.html

---
+++ scripts/init-cards.js
@@ -0,0 +1,47 @@
+const initGallery = [
+  {
+    number: "1",
+    region: "Region™",
+    result: "Result",
+  },
+  {
+    number: "2",
+    region: "Region™",
+    result: "Result",
+  },
+  {
+    number: "3",
+    region: "Region™",
+    result: "Result",
+  },
+];
+
+//инициализация карточек каталог
+
+const itemTemplate = document.getElementById("galleryGrid").content;
+const elements = document.querySelector(".gallery__grid");
+
+function createCard(item) {
+  const htmlElement = itemTemplate.cloneNode(true);
+  const caseNumber = htmlElement.querySelector(".case-number");
+  const caseRegion = htmlElement.querySelector(".case-region");
+  const caseResult = htmlElement.querySelector(".case-result");
+
+  // Проверяем наличие значения поля в объекте item перед присваиванием его элементу
+  if (item.number) {
+    caseNumber.textContent = item.number;
+  }
+  if (item.region) {
+    caseRegion.textContent = item.region;
+  }
+  if (item.result) {
+    caseResult.textContent = item.result;
+  }
+
+  return htmlElement;
+}
+
+initGallery.forEach(function (item) {
+  const elementCreate = createCard(item);
+  elements.append(elementCreate);
+});
