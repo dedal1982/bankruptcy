db55fd1df3d2f19052b3f2020b7f5f868e530b61
Al Dm <d44.a1@yandex.ru>
2025-12-04 18:10:28 +0300

    update index.html, init-cards.js, gallery.css and header.css

--- scripts/init-cards.js
+++ scripts/init-cards.js
@@ -1,23 +1,51 @@
 const initGallery = [
   {
-    number: "1",
-    region: "Region™",
-    result: "Result",
+    number: "А05-7344/2023",
+    region: "Архангельская область",
+    date: "20.10.2025",
+    result: "завершено освобождением гражданина от долгов",
   },
   {
-    number: "2",
-    region: "Region™",
-    result: "Result",
+    number: "А05-1455/2025",
+    region: "Архангельская область",
+    date: "05.11.2025",
+    result: "завершено освобождением гражданина от долгов",
   },
   {
-    number: "3",
-    region: "Region™",
-    result: "Result",
+    number: "А05-14146/2024",
+    region: "Архангельская область",
+    date: "28.05.2025",
+    result: "завершено освобождением гражданина от долгов",
   },
   {
-    number: "4",
-    region: "Region™",
-    result: "Result",
+    number: "А05-1836/2025",
+    region: "Архангельская область",
+    date: "25.11.2025",
+    result: "завершено освобождением гражданина от долгов",
+  },
+  {
+    number: "А13-1842/2025",
+    region: "Вологодская область",
+    date: "02.10.2025",
+    result: "завершено освобождением гражданина от долгов",
+  },
+  {
+    number: "А13-1531/2025",
+    region: "Вологодская область",
+    date: "17.11.2025",
+    result: "завершено освобождением гражданина от долгов",
+  },
+  {
+    number: "А13-713/2025",
+    region: "Вологодская область",
+    date: "27.08.2025",
+    result: "завершено освобождением гражданина от долгов",
+  },
+  {
+    number: "А29-2820/2024",
+    region: "Республика Коми",
+    date: "27.10.2024",
+    result: "завершено освобождением гражданина от долгов",
   },
 ];
 
@@ -31,6 +59,7 @@ function createCard(item) {
   const caseNumber = htmlElement.querySelector(".case-number span");
   const caseRegion = htmlElement.querySelector(".case-region span");
   const caseResult = htmlElement.querySelector(".case-result span");
+  const caseDate = htmlElement.querySelector(".case-date span");
 
   // Проверяем наличие значения поля в объекте item перед присваиванием его элементу
   if (item.number) {
@@ -38,6 +67,9 @@ function createCard(item) {
   }
   if (item.region) {
     caseRegion.textContent = item.region;
+  }
+  if (item.date) {
+    caseDate.textContent = item.date;
   }
   if (item.result) {
     caseResult.textContent = item.result;
